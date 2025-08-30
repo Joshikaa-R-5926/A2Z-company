81e7484e416b8b1cb56ead902b411c83a248cee3
[dyad] <git@dyad.sh>
2025-08-30 17:00:25 +0530

    [dyad] Updated the homepage hero image - wrote 1 file(s)

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -85,9 +85,9 @@ const Home = () => {
                 </div>
               </div>
               <img
-                alt="Abstract futuristic background representing innovation"
+                alt="A close-up view of a glowing computer circuit board"
                 className="mx-auto aspect-video overflow-hidden rounded-xl object-cover sm:w-full lg:order-last lg:aspect-square"
-                src="https://images.unsplash.com/photo-1550439062-609e1531270e?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+                src="https://images.unsplash.com/photo-1518770660439-4636190af475?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
               />
             </div>
           </div>
